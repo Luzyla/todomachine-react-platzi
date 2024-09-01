85163ef8dd270f86255c49431cfdf1196f6d610f
Luzyla <luzyla@gmail.com>
2024-09-01 03:08:01 +0200

    Add styles: Add hover.

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,9 +1,12 @@
-function TodoItem({ text, completed }) {
+function TodoItem(props) {
+  //tambien podria poner directamente las propiedes desestructuradas {text, completed}
   return (
     <li className="container-task flex centrar-x">
-      <div className="pixel"></div>
-      <p className="task-text">{text}</p>
-      <span className="pixel del-task flex"></span>
+      <div className={`pixel  ${props.completed && "pixel--active"}`}>V</div>
+      <p className={`task-text ${props.completed && "task-text--completed"}`}>
+        {props.text}
+      </p>
+      <span className="pixel del-task flex">X</span>
     </li>
   );
 }
